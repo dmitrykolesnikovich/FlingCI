293dc399bc427858bd38b1a9f9bb58ec7f546b90
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2019-09-20 15:28:58 -0400

    Move the build containers to a seperate repo and make the jenkins container have blue ocean

--- jenkins/catPasswd.sh
+++ jenkins/catPasswd.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-docker exec -it jenkins_jenkins_1 cat /var/jenkins_home/secrets/initialAdminPassword
+docker exec -it fling_jenkins_jenkins_1 cat /var/jenkins_home/secrets/initialAdminPassword
