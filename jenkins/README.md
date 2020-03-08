2271b0e9ec91af7f127a09bca19010baa9196c67
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2020-03-08 17:49:09 -0400

    Update readme and comment blue ocean script

--- jenkins/README.md
+++ jenkins/README.md
@@ -2,3 +2,6 @@
 
 `Warning` : this is not finished! This docker compose will build and 
 run a Jenkins docker instance, but the jobs do not yet build the Fling engine. 
+
+# Running
+Run `make run` in this folder to build our Jenkins pipeline
--- jenkins/run-blue-ocean.sh
+++ jenkins/run-blue-ocean.sh
@@ -1,2 +1,5 @@
 #/bin/bash
+# Runs jenksing on port 8080, creates a linked jenkins data folder 
+# to this host machine, and attachs the docker socket so 
+# jenkins can run docker jobs
 docker run -u root --rm -d -p 8080:8080 -p 50000:50000 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkinsci/blueocean
